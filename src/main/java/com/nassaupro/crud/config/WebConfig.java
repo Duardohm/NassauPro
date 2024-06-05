6a06c2847e9861255a643b09b2d4939c585d857d
Eduardo <edu.pereira3355@gmail.com>
2024-06-05 15:13:18 -0300

    atualização no cors

--- src/main/java/com/nassaupro/crud/config/WebConfig.java
+++ src/main/java/com/nassaupro/crud/config/WebConfig.java
@@ -7,7 +7,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-    	registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
